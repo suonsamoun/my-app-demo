6fde2e1ee9ebb90ca40603155a5709d72ca96a12
suonsamoun <suonsamoun@gmail.com>
2023-12-11 21:13:49 +0700

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <iframe
+    src="https://demo.templatemonster.com/demo/78185.html"
+    frameborder="0"
+    style="height: 100vh; width: 100%; margin-top: -70px"
+    ref="myFrame"
+    id="myFrame"
+  ></iframe>
+</template>
